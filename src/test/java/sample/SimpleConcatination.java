bd2d6981ada80ce8a98cbbb9c07b74c1719c0415
Vishnupriya Jorrigala <visjorri@publicisgroupe.net>
2022-11-29 22:28:10 +0530

    file added

--- src/test/java/sample/SimpleConcatination.java
+++ src/test/java/sample/SimpleConcatination.java
@@ -8,13 +8,10 @@ public class SimpleConcatination {
 @Test
 
 	public void method1() {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter first name:");
-		String str1=sc.nextLine();
-		System.out.println("enter last name:");
-		String str2=sc.nextLine();	
-		String s=str1+str2;
-		System.out.println("Hello"+" "+s);
+		String firstname="vishnupriya";
+		String lastname="jorrigala";
+    String s=firstname+lastname;
+    System.out.println("Hello"+" "+s);
 
 	}
 
